8d3a384b3e4ba5fb29801db99b1607bfe87cef6a
Akhila-Kurapati <akhilakurapati0123@gmail.com>
2026-02-09 15:33:05 +0530

    updates portfolio design

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     skill.style.animationDelay = `${index * 0.2}s`;
   });
 });
+// Toggle menu on click
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
